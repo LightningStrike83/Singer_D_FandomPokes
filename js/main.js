c00522b3c38682a1cc89ea389394e932db1c97fb
Dan Singer <d_singer@fanshaweonline.ca>
2025-02-13 00:18:45 -0500

    Suggestion Page Rules
    -Wrote a rules section to ensure no misuse of website
    -Added checkboxes for acceptance
    -Added guarantee functionality
    -Added functionality for easier click after first acceptance via LocalStorage check

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 import { darkmode } from "./modules/dark-mode.js";
 import { populationFunctionality } from "./modules/home.js";
 import { lightboxFunctions } from "./modules/lightbox.js";
+import { ruleCheck } from "./modules/rules.js";
 import { smoothScroll } from "./modules/scroll.js";
 
 if (document.body.dataset.page === "home") {
     populationFunctionality()
+}
+
+if (document.body.dataset.page === "suggest") {
+    ruleCheck()
 }
 
 smoothScroll()
