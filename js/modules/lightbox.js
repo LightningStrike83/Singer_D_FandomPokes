ef64a35fbd495160db08f4a589465150d4d382a6
Dan Singer <d_singer@fanshaweonline.ca>
2025-02-23 22:20:09 -0500

    Update Information
    -Added a lightbox for site updates
    -Functionality for display for new lightbox
    -Styling for new functionality
    -Minor script fix

--- js/modules/lightbox.js
+++ js/modules/lightbox.js
@@ -8,7 +8,7 @@ export function lightboxFunctions() {
 
         if (lightbox.style.opacity === "0") {
             lightbox.style.opacity = "1"
-        } else if (lightbox.style.opacity = "1") {
+        } else if (lightbox.style.opacity === "1") {
             lightbox.style.opacity = "0"
         }
 
